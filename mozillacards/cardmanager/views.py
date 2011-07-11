951bbb7c910cbe44fb3daae2e93def417aa2cb79
Giorgos Logiotatidis <seadog@sealabs.net>
2011-07-11 12:25:26 +0300

    enabling csrf checks

--- mozillacards/cardmanager/views.py
+++ mozillacards/cardmanager/views.py
@@ -1,6 +1,12 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
 
 def index(request):
-    return render_to_response("index.html")
+    if request.method == 'GET':
+        c = {}
+        c.update(csrf(request))
+        return render_to_response("index.html", c)
 
+    else:
+        return render_to_response("index.html")
